c540d7e6181a71a77001d6ef80fb7f31d8e09baf
Ruben Bert Pingol <rubenberttpingol@gmail.com>
2020-06-03 02:32:00 +0800

    [upd] constants comment for better understanding of countdown timer value

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
 export const ENV = process.env.NODE_ENV || "development";
 export const API_PATH =
   process.env.API_PATH || "https://warm-ravine-50741.herokuapp.com";
-export const COUNTDOWN_TIMER_VALUE = 16200000; // 4.5 * 24 * 60 * 60 * 1000;
+export const COUNTDOWN_TIMER_VALUE = 16200000; // 4.5 * 60 * 60 * 1000;
